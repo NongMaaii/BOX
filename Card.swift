c69625f67f9f7fd54f5ef616176f56ec50799ab4
NongMaaii <80394537+NongMaaii@users.noreply.github.com>
2025-05-04 15:44:20 +0700

    Update Card.swift

--- Card.swift
+++ Card.swift
@@ -48,7 +48,7 @@ class Card {
     
     // ฟังก์ชั่นแสดงข้อมูลไพ่
     func description() -> String {
-        return "
+        return " "
 
         struct Card: Identifiable {
     let id = UUID()
