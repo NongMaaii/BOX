4ef3ccd6d28980b7531514f289020b35b1c74d48
NongMaaii <80394537+NongMaaii@users.noreply.github.com>
2025-05-04 12:27:30 +0700

    Update Deck.swift

--- Deck.swift
+++ Deck.swift
@@ -86,3 +86,20 @@ class Deck {
         return cards.popLast()
     }
 }
+
+struct Deck {
+    var cards: [Card] = []
+
+    init() {
+        for suit in Suit.allCases {
+            for rank in Rank.allCases {
+                cards.append(Card(suit: suit, rank: rank))
+            }
+        }
+        cards.shuffle()
+    }
+
+    mutating func dealCard() -> Card? {
+        return cards.popLast()
+    }
+}
